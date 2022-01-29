249a1e4949b678623532af909ff0ee3f5a0380d9
dparikesit <dsparikesit@gmail.com>
2022-01-29 23:26:30 +0700

    Add Webhook Event Type

--- utils/line.go
+++ utils/line.go
@@ -9,6 +9,31 @@ import (
 var Bot *linebot.Client
 var err error
 
+type WebhookEvent struct {
+	Destination string `json:"destination"`
+	Events      Event  `json:"events"`
+}
+
+type Event struct {
+	Type       string       `json:"type"`
+	Timestamp  int          `json:"timestamp"`
+	Source     EventSource  `json:"source"`
+	ReplyToken string       `json:"reply_token,omitempty"`
+	Mode       string       `json:"mode,omitempty"`
+	Message    EventMessage `json:"message,omitempty"`
+}
+
+type EventSource struct {
+	Type   string `json:"type"`
+	UserId string `json:"user_id"`
+}
+
+type EventMessage struct {
+	Type string `json:"type"`
+	Id   string `json:"id"`
+	Text string `json:"text"`
+}
+
 func ConnectLine() {
 	Bot, err = linebot.New(os.Getenv("CHANNELSECRET"), os.Getenv("CHANNELTOKEN"))
 	if err != nil {
