d21e649f0749c7d2eeeab34ee4a8ddac99fd342c
dparikesit <dsparikesit@gmail.com>
2022-01-29 22:54:25 +0700

    Test message

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,11 +1,18 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gofiber/fiber/v2"
-	"log"
 )
 
 func Line(c *fiber.Ctx) error {
-	log.Println(c.Body())
+	var result map[string]interface{}
+	err := json.Unmarshal(c.Body(), &result)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(result)
 	return c.SendString("Halo")
 }
